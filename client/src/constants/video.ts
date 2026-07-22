3ab10517812561f8ce0a693f42a455290ec83bf9
AstraOnlineWeb <astraonlineweb@gmail.com>
2026-07-22 16:16:41 -0300

    Volta a banda de vídeo para 160x120 @ 120 kbps (prioriza fluidez)
    O aumento para 240x180 @ 250 kbps deixou a imagem mais nítida mas adicionou
    latência perceptível (mais dados no relay). Como a prioridade é fluidez/baixo
    delay, volta ao patamar leve.

--- client/src/constants/video.ts
+++ client/src/constants/video.ts
@@ -1,7 +1,7 @@
 export const H264_CHANNEL_LABEL = "h264";
 export const VIDEO_CODEC = "avc1.42E01F";
-export const VIDEO_WIDTH = 240;
-export const VIDEO_HEIGHT = 180;
+export const VIDEO_WIDTH = 160;
+export const VIDEO_HEIGHT = 120;
 export const VIDEO_FPS = 15;
-export const VIDEO_BITRATE = 250_000;
+export const VIDEO_BITRATE = 120_000;
 export const VIDEO_KEYFRAME_INTERVAL = VIDEO_FPS;
